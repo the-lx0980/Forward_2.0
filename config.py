bb8c8753a739b887a8d13e9f2471153288fbc416
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-06 00:14:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     
     API_ID = 16448144
     API_HASH = "1073665850700150caf0e0cbb68216a2"
-    BOT_TOKEN = "6042686351:AAFcvKuNs4k5IQSYc5xE3WGwnzm6E0a3K4U"
+    BOT_TOKEN = "6042686351:AAE_USR81Wy0Km66NKKSCEjQiU50du2eebc"
     BOT_SESSION = "Rentrox_Forward_BOT"
     OWNER_ID = "5163706369"
     DATABASE_URI = "mongodb+srv://Boyuserrentrox:mg1BbXIGzcqXXTJG@cluster0.l1xnuaj.mongodb.net/?retryWrites=true&w=majority"
