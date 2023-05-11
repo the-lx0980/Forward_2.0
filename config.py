0f38edeb3a9663f2b7faf12ab0c75a749c2e887f
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-11 19:52:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     DATABASE_URI = "mongodb+srv://Boyuserrentrox:mg1BbXIGzcqXXTJG@cluster0.l1xnuaj.mongodb.net/?retryWrites=true&w=majority"
     DATABASE_NAME = "Cluster0"
     COLLECTION_NAME = 'Rentrox_Forward_data'
-    SESSION = "BQAO68s2Ny4y6_5NpGP3ZVWcYICpufNpc9nf0AAdZTaITWiDO6wk4Xb2alAeV-EBwNS9f9TRUeYr5N8X2OcYJ0tWB2RBQhP4d6sI9z-cJqBVOIvCBvjteHMI1NDV_cckVsHI2Mwo0hMPqXFnzseNFDIPHjduRSb2yQl6Zxw1O7TRJTHEwHcRoiP3zAMRXp7XfK52islUXfWXL0Ie2cgYEm7t1dPkUWHiDBNf5eStLWIQEPQwVB0WicttyLZQTgEidzlIQpsrsI3dQ-Xa0Hshyqch49JEi-F3zyqEyaioj6d0hLNxCTbw1AvSlyMl01QgXOADFx06CjfcamKWC612Aq7aAAAAAW8HBfYA"
+    SESSION = os.environ.get("SESSION")
     TO_CHANNEL = -1001559863637
     BOT_USERNAME= "RentroxAutoCloneBot"
 
